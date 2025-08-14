4ef1ab8fdaf0cd66423602c55b0df207c7ebb301
Atanas Ginev <atanas.ginev@gmail.com>
2025-08-14 11:33:56 +0300

    Removed some boilerplate

--- src/routes/orders.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-import type { Request, Reply } from '../types';
-
-export async function orderRoutes(app: FastifyInstance) {
-    app.get('/', async (req: Request, reply: Reply) => {
-        return reply.send({ orders: [] });
-    });
-}
--- src/routes/products.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-import type { Request, Reply } from '../types';
-
-export async function productRoutes(app: FastifyInstance) {
-    app.get('/', async (req: Request, reply: Reply) => {
-        return reply.send({ products: [] });
-    });
-}
